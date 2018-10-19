9cc99ba7d396e4a743e63b8a6af90980c890f625
dhowes24 <d.howes24@gmail.com>
2018-10-19 12:05:41 -0400

    Navigation
    Navigation and beginning of Capturescreen with component Communcaiton

--- App.js
+++ App.js
@@ -1,17 +1,78 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet,Button, Text, View } from 'react-native';
+import { createStackNavigator } from 'react-navigation';
 
-export default class App extends React.Component {
+
+class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
+        <Image
+            style={HomeBackground.container}
+            source={}
+      <View style={HomeBackground.container}>
         <Text>Open up App.js to start working on your app!</Text>
+          <Button
+              title="New Receipt"
+              onPress={() => this.props.navigation.navigate('CaptureScreen')}
+          />
+          <Button
+              title="Pending Receipt"
+              onPress={() => this.props.navigation.navigate('ReceiptsPage')}
+          />
+          <Button
+              title="Account"
+              onPress={() => this.props.navigation.navigate('AccountPage')}
+          />
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
+class ReceiptsPage extends React.Component {
+    render() {
+        return (
+            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                <Text> ReceiptsPage </Text>
+            </View>
+        );
+    }
+}
+class AccountPage extends React.Component {
+    render() {
+        return (
+            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                <Text> AccountPage </Text>
+            </View>
+        );
+    }
+}
+class CaptureScreen extends React.Component {
+    render() {
+        return (
+            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                <Text> CaptureScreen </Text>
+            </View>
+        );
+    }
+}
+
+export default createStackNavigator({
+    Home: {
+        screen: App,
+    },
+    CaptureScreen: {
+        screen: CaptureScreen,
+    },
+    ReceiptsPage: {
+        screen: ReceiptsPage,
+    },
+    AccountPage: {
+        screen: AccountPage,},
+}, {
+    initialRouteName: 'Home',
+});
+
+const HomeBackground = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
