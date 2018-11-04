bcf6213a63b8ef41436f41d2b97a779664e283ea
dhowes24 <d.howes24@gmail.com>
2018-11-04 14:34:57 -0500

    Intial CheckPlease Commit

--- Components/CaptureScreen.js
+++ Components/CaptureScreen.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import { StyleSheet,Button, Text, View, TouchableOpacity } from 'react-native';
 import { createStackNavigator } from 'react-navigation';
+import axios from 'axios';
 
 import { Camera, Permissions, FileSystem } from 'expo';
+
+//API KEY
+const cloudVisionKey ='AIzaSyAubtQznQNsxlTo-jU79XtShR8_Iz_wPaI';
+
+//ENDPOINTS
+const cloudvision = 'https://vision.googleapis.com/v1/images:annotate?key=' + cloudVisionKey;
+
+//BUCKETS
+const imageBucket = 'checkpleaseimagebucket';
 
 export default class CaptureScreen extends React.Component {
     state = {
         hasCameraPermission: null,
         type: Camera.Constants.Type.back,
-        actualPhoto: null,
     };
 
     async componentWillMount() {
@@ -20,10 +29,72 @@ export default class CaptureScreen extends React.Component {
         console.log('pressed');
         if (this.camera) {
             let photo = await this.camera.takePictureAsync();
-            console.log(photo.uri);
-            this.setState({actualPhoto: photo})
+            this.detectText(photo)
         }
     };
+
+
+
+    //Vision Area
+
+    detectText(photo) {
+        this.uploadPhoto(photo);
+        console.log('made it');
+        let body = {
+            "requests": [
+                {
+                    "image": {
+                        "source": {
+                            "imageUri": "gs://"+imageBucket+photo.uri
+                        } //Needs to be either, 64-bit (Can't fucking do in expo), public URL, or in Google storage
+
+                    },
+                    "features": [
+                        {
+                            "type": "TEXT_DETECTION",
+                            "maxResults": 1
+                        }
+                    ]
+                }
+            ]
+        }
+
+        axios.post(cloudvision, body)
+            .then(function (response) {
+                console.log(response.data)
+            })
+    }
+
+    //TODO: Fix for this importing Node Stand Library Mod "Stream", Isnt supported by React Native
+    uploadPhoto(photo){
+            // format the image data
+            const image = {
+                uri: photo.uri,
+                type: 'image/jpeg',
+                name:'Check',
+            };
+            // Instantiate a FormData() object
+            const imgBody = new FormData();
+            const url = `/Router`;
+            // Perform the request. Note the content type - very important
+            fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'multipart/form-data',
+                },
+                body: imgBody
+            }).then(res => res.json()).then(results => {
+                // Just me assigning the image url to be seen in the view
+                const source = { uri: photo.uri, isStatic: true };
+                const images = this.state.images;
+                images[index] = source;
+                this.setState({ images });
+            }).catch(error => {
+                console.error(error);
+            });
+        };
+
 
     renderCamera = () =>
         (
@@ -70,5 +141,5 @@ export default class CaptureScreen extends React.Component {
                 this.renderCamera()
             );
         }
+    };
     }
-}
---
+++ nodejs-assets/build-native-modules-MacOS-helper-script-node.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+      # Helper script for Gradle to call node on macOS in case it is not found
+      export PATH=$PATH:/Users/derekhowes/.nvm/versions/node/v11.0.0/lib/node_modules/npm/node_modules/npm-lifecycle/node-gyp-bin:/Users/derekhowes/CheckPleaseNative/node_modules/nodejs-mobile-react-native/node_modules/.bin:/Users/derekhowes/CheckPleaseNative/node_modules/.bin:/Users/derekhowes/Downloads/google-cloud-sdk/bin:/Users/derekhowes/.nvm/versions/node/v11.0.0/bin:/Library/Frameworks/Python.framework/Versions/3.6/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/mongodb/bin
+      node $@
+    
