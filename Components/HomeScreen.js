de343d338af657918d6934586adcb9d843b060b9
dhowes24 <d.howes24@gmail.com>
2018-12-15 18:25:43 -0500

    FlatList and HomePageUI

--- Components/HomeScreen.js
+++ Components/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet,Button, Text, View } from 'react-native';
+import {StyleSheet, Button, Text, View, ImageBackground, Image, TouchableOpacity} from 'react-native';
 import CaptureScreen from './CaptureScreen';
 
 
@@ -10,20 +10,42 @@ export default class HomeScreen extends React.Component {
 
     render() {
         return (
+            <ImageBackground source={require('../assets/Home_Page_Background.png')}
+                             style={{width: '100%', height: '100%'}}>
+                <Image
+                    source={require('../assets/Home_Page_Logo.png')}
+                    style={{alignContent:'center', marginTop: '10%', marginLeft: '17.5%', width: '65%', height:'35%'}}>
+                </Image>
             <View style={HomeBackground.container}>
-                <Button
-                    title="New Receipt"
+                <View style={HomeBackground.buttonBorder}>
+                <TouchableOpacity
                     onPress={() => this.props.navigation.navigate('CaptureScreen')}
-                />
-                <Button
-                    title="Pending Receipt"
-                    onPress={() => this.props.navigation.navigate('PendingMeals')}
-                />
-                <Button
-                    title="Account"
-                    onPress={() => this.props.navigation.navigate('AccountPage')}
-                />
+                >
+                    <Text style={HomeBackground.clickableText}>
+                        New Receipt
+                    </Text>
+                </TouchableOpacity>
+                </View>
+                <View style={HomeBackground.buttonBorder}>
+                    <TouchableOpacity
+                        onPress={() => this.props.navigation.navigate('PendingMeals')}
+                    >
+                        <Text style={HomeBackground.clickableText}>
+                            Pending Receipt
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={HomeBackground.buttonBorder}>
+                    <TouchableOpacity
+                        onPress={() => this.props.navigation.navigate('AccountPage')}
+                    >
+                        <Text style={HomeBackground.clickableText}>
+                            Account
+                        </Text>
+                    </TouchableOpacity>
+                </View>
             </View>
+            </ImageBackground>
         );
     }
 }
@@ -31,8 +53,21 @@ export default class HomeScreen extends React.Component {
 const HomeBackground = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
+        marginTop:'-60%'
     },
+    buttonBorder:{
+        borderColor:'brown',
+        borderWidth: 3,
+        width:'65%',
+        height:'7%',
+        alignContent: 'center',
+        alignItems: 'center',
+        marginTop: '3%'
+    },
+    clickableText:{
+        fontSize: 20,
+        marginTop: '5%'
+    }
 });
--- Components/PendingMeals.js
+++ Components/PendingMeals.js
@@ -1,4 +1,6 @@
-import {Image, ImageBackground, StyleSheet, View} from "react-native";
+import {Image, ImageBackground, StyleSheet, View, FlatList, ActivityIndicator} from "react-native";
+
+
 
 import {
     Container, Content,
@@ -7,13 +9,42 @@ import {
 import React from "react";
 import PendingMealsCards from './PendingMealsCards'
 import TouchableItem from "react-navigation/src/views/TouchableItem";
+import {List , ListItem} from "react-native-elements"
 
 
 export default class PendingMeals extends React.Component {
 state={
-    owned: true
+    owned: true,
+    ownedData: [{EventName: 'PoolParty', DateTime: '4/20 - 4:20', EventID: 1}, {EventName: 'PoolParty', DateTime: '4/23 - 4:20', EventID: 1}],
+    unOwnedData: [],
+    loading:true
+
 };
 
+componentDidMount() {
+    //Make Request for Owned & UnOwned Data
+        //Put into respective arrays
+
+
+    //if current Query Data is NUll... which will be set to null upon entry on page
+    //Query Database for
+    //Select * from events where AdminContact = 781; -- for owned Meals
+    //Select Events.EventID, Events.Data, Events.AdminContact from events left join Participants on Participants.EventID = Events.EventID Where Participants.Contact = 781 and Events.AdminContact!= 781;
+    //781 is the CONTACT info
+    //Else just use old Query Data for respective Toggle
+
+
+    this.setState({loading:false})
+}
+
+renderFooter = () => {
+    if (!this.state.loading) return null;
+    return(
+        <View>
+            <ActivityIndicator animating size={"large"}/>
+        </View>
+    )
+};
     render() {
         return (
             <ImageBackground source={require('../assets/Mobile_background.png')}
@@ -45,27 +76,19 @@ state={
                     flex: 1,
                     backgroundColor: 'white',
                 }}>
-                    <PendingMealsCards Meal={'Pool Party'} DateTime={'4/20 - 4:20'}/>
-                    <PendingMealsCards/>
-                    <PendingMealsCards/>
-                    <PendingMealsCards/>
-                    <PendingMealsCards/>
-                    <PendingMealsCards/>
-                    <PendingMealsCards/>
-                    <PendingMealsCards/>
+                    <List containerStyle={{borderTopWidth:0, borderBottomWidth:0, marginTop: 0}}>
+                        <FlatList
+                        data={this.state.owned ? this.state.ownedData : this.state.unOwnedData}
+                        renderItem={({ item }) => (
+                            <PendingMealsCards Meal={item.EventName} DateTime={item.DateTime}/>
+                        )}
+                        keyExtractor={item => item.EventID}
+                        ListFooterComponent={this.renderFooter()}/>
+                    </List>
                 </Content>
 
             </ImageBackground>
         )
-    }
-
-    findMeals(){
-     //if current Query Data is NUll... which will be set to null upon entry on page
-        //Query Database for
-            //Select * from events where AdminContact = 781; -- for owned Meals
-            //Select Events.EventID, Events.Data, Events.AdminContact from events left join Participants on Participants.EventID = Events.EventID Where Participants.Contact = 781 and Events.AdminContact!= 781;
-            //781 is the CONTACT info
-     //Else just use old Query Data for respective Toggle
     }
 }
 const styles = StyleSheet.create({
